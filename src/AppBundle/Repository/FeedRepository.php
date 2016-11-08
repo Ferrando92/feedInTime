6f3c56496c020d26f63136b1256d8debf0fa6f10
Sergio Ferrando <immawinnerbaby92@gmail.com>
2016-11-08 14:34:54 +0100

    Update FeedRepository.php

--- src/AppBundle/Repository/FeedRepository.php
+++ src/AppBundle/Repository/FeedRepository.php
@@ -6,7 +6,7 @@ class FeedRepository extends EntityRepository
     private function findActivasEnPortada()
     {
       $em = $this->getDoctrine()->getManager();
-      return $em->getRepository('AppBundle:Feed')->findBy(array('activa_en_portada' => true));
+      return $em->getRepository('AppBundle:Feed')->findBy(array('active_at_frontpage' => true));
     }
 }
 
